4c08e399b5e6c4c5ef8aa49ba28765c85abe858b
maxarrowood <112727066+maxarrowood@users.noreply.github.com>
2023-01-23 14:43:31 +0000

    update

--- prog54c.py
+++ prog54c.py
@@ -4,7 +4,7 @@ pi = 3.14159
 rad = int(input("Enter a Radius: "))
 
 area = pi * rad ** 2
-circum = 2 * pi * r
+circum = 2 * pi * rad
 
 print ("Area of Circle: " + str(area))
 print ("Circum of Circle: " + str(circum))
