e7d2f241aea9a91f5695f41d28a596792d930e4f
maxarrowood <112727066+maxarrowood@users.noreply.github.com>
2023-01-23 14:37:22 +0000

    update

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 print("Hello, world!")
+# this is a single line comment
+
+"""This is a 
+multi-line comment"""
+
+""" Arithmatic Operators
++ Add
+- Subtract
+* Multiply
+/ Divide
+// Floor Division
+% Modulo (Remainder)
+** Pow
+"""
+
+""" Assignment Operators:
+= Value Assingments (x = y)
++= Add to (x += y is same as  x = x + y)
+-= Subtract from (x -= y is same as x = x - y)
+*= Multiply by (x *= y is same as x = x * y)
+/= Divide by (x /= y is same as x = x / y)
+//= Floor Division (x //= y is same as x = x // y)
+%= Modulo (x %= y is same as x = x % y)
+**= Power by (x **= y is same as x = x ** y)
+"""
+
+""" Data Types:
+int / Integer, Whole Number
+float / Floating Point, Number w/ Decimal
+str / String, any text
+bool / Boolean, true or false
+chr - Character, any single character
+list - List, a collection of values
+"""
---
+++ prog52a.py
@@ -0,0 +1,10 @@
+# Lang52a - Area and Perim of Rectangle
+
+length = int(input("Enter the length of the rectangle: "))
+width = int(input("Enter the width of the rectangle: "))
+
+area = length * width
+perim = 2 * (length + width)
+
+print("The area of the rectangle is: " + str(area))
+print("The perimeter of the rectangle is: " + str(perim))
