73c86335e38db4456e3d96d76ae7bf5d68a8af51
maxarrowood <112727066+maxarrowood@users.noreply.github.com>
2023-02-20 14:29:55 +0000

    update

---
+++ cmdlineargs.py
@@ -0,0 +1,14 @@
+import sys  # acess to command line args
+
+
+def main(args):
+  if len(args) <= 0:
+    print("Hello!")
+    return
+  print("Hello,", args[0])
+  for arg in args:
+    print(arg)
+
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
