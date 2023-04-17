7791e984e8a4a3c06141847b55ce9568d1814d09
maxarrowood <112727066+maxarrowood@users.noreply.github.com>
2023-04-17 08:46:35 -0500

    update

---
+++ Solution1/AboutMe/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AboutMe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "My name is Max Arrowood. My favorite sport is basketball and my favorite quote is “To infinity and beyond!” - Buzz Lightyear";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
