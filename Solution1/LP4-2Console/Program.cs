46030b75f1ea8d2f9c630fde0be8b4664c1166be
maxarrowood <112727066+maxarrowood@users.noreply.github.com>
2023-04-20 08:33:01 -0500

    update

---
+++ Solution1/LP4-2Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP4_2Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Weight: ");
+            int weight = int.Parse(Console.ReadLine());
+            Console.Write("Enter Length: ");
+            int length = int.Parse(Console.ReadLine());
+            Console.Write("Enter Width: ");
+            int width = int.Parse(Console.ReadLine());
+            Console.Write("Enter Height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            int volume = length * width * height;
+
+            if (weight > 27 && volume > 100000)
+            {
+                Console.WriteLine("Package is too heavy and too large.");
+            }
+            else if (weight > 27)
+            {
+                Console.WriteLine("Package is too heavy.");
+            }
+            else if (volume > 100000)
+            {
+                Console.WriteLine("Package is too large.");
+            }
+            else
+            {
+                Console.WriteLine("Package is within parameters.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Solution1/Prog535Console/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog535Console
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(75, 375);
+            int num2 = rnd.Next(75, 275);
+
+            button1.Location = new Point(num, num2);
+        }
+    }
+}
