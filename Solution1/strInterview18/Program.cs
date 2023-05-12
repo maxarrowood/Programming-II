33f97a8962d4e0a281c860c3f799dbdaab0be3f0
maxarrowood <112727066+maxarrowood@users.noreply.github.com>
2023-05-12 08:42:44 -0500

    update

---
+++ Solution1/strInterview18/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strInterview18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string text = Console.ReadLine();
+            Console.Write("Enter a character to remove: ");
+            string cha = Console.ReadLine();
+
+            string result = searchText(text, cha);
+            Console.WriteLine("The new string is: " + result);
+            Console.ReadLine();
+        }
+
+        static string searchText(string text, string cha)
+        {
+            int tLen = text.Length;
+
+            for (int lcv = 0; lcv <= tLen; lcv++)
+            {
+                if (text.Substring(lcv) == cha)
+                {
+                    text.Remove(lcv);
+                }
+            }
+            return text;
+        }
+    }
+}
---
+++ Solution1/strInterview8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strInterview8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string text = Console.ReadLine();
+            Console.Write("Enter a character to search for: ");
+            string cha = Console.ReadLine();
+
+            int result = searchText(text, cha);
+            Console.WriteLine("Does \"" + text + "\" contain \"" + cha + "\"?: " + result);
+            Console.ReadLine();
+        }
+
+        static int searchText(string text, string cha) {
+            int tLen = text.Length;
+            int x = 0;
+
+            for (int lcv = 0; lcv <= tLen; lcv++) {
+                if (text.Substring(lcv) == cha) {
+                    x++;
+                }
+            }
+            return x;
+        }
+    }
+}
